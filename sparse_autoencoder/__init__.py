b9c0cf01c2c1a611a835a6ceed3785d9d06f279f
TushaarGVS <tushaargvsg45@gmail.com>
2024-07-29 15:57:00 -0400

    Enable distributed data processing

---
+++ sparse_autoencoder/__init__.py
@@ -0,0 +1,8 @@
+# Adapted from OpenAI sparse autoencoder repository:
+# https://github.com/openai/sparse_autoencoder.
+
+__version__ = "0.1.0"
+
+from sparse_autoencoder import model
+
+AutoEncoder = model.AutoEncoder
